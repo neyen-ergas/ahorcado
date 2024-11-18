160a6d0de7f7548927237546a3929b6a0a5e7007
chuli03 <bieber676@gmail.com>
2024-11-18 15:45:56 -0300

    dibujitos del tipito y play again

--- inicio.py
+++ inicio.py
@@ -1,6 +1,7 @@
 import random
 from dificultad import *
 from categorias import *
+from intentos import *
 import dificultad as dificultades
 
 def elegir_dificultad():
@@ -44,48 +45,58 @@ def actualizar_guiones(guiones, palabra, letra):
 def intentos_a_corazones(intentos):
     corazones=""
     while intentos>0:
-        corazones=corazones + "♥ "
+        corazones=corazones + "❤ "
         intentos-= 1
     return corazones
 
 
 
 def juego():
-    print("Bienvenido al juego del ahorcado")
-    
-    dificultad = elegir_dificultad()
-    palabra = elegir_palabra(dificultad)
-    guiones = mostrar_guiones(palabra)
-    intentos = 5 
-    letras_adivinadas = []
-    
-    print(f"Palabra a adivinar: {guiones}")
-    
-    while intentos > 0:
-        letra = pedir_letra()
+    print("Bienvenido al juego del ahorcado!")
 
-        if letra in letras_adivinadas:
-            print("Ya adivinaste esa letra, intenta con otra.")
-            continue
+    while True:
+        dificultad = elegir_dificultad()
+        palabra = elegir_palabra(dificultad)
+        guiones = mostrar_guiones(palabra)
+        intentos = 5
+        letras_adivinadas = []
 
-        letras_adivinadas.append(letra)
-        
-        if letra in palabra.lower():
-            print(f"¡Correcto! La letra {letra} está en la palabra.")
-            guiones = actualizar_guiones(guiones, palabra, letra)
-            print(f"Palabra actual: {guiones}")
+        print(f"¡A jugar!🎮\nPalabra a adivinar: {guiones}")
+
+        while intentos > 0:
+            letra = pedir_letra()
+
+            if letra in letras_adivinadas:
+                print("Ya adivinaste esa letra, intenta con otra.")
+                continue
+
+            letras_adivinadas.append(letra)
+
+            if letra in palabra.lower():
+                print(f"¡Correcto! La letra {letra} está en la palabra.")
+                guiones = actualizar_guiones(guiones, palabra, letra)
+                print(f"Palabra actual: {guiones}")
+            else:
+                intentos -= 1
+                corazones = intentos_a_corazones(intentos)
+                mostrar_ahorcado(intentos)
+                print(f"¡Letra incorrecta! Te quedan {corazones} vidas.")
+
+
+            if "_ " not in guiones:
+                print(f"¡Felicidades! Has adivinado la palabra: {palabra}")
+                break
         else:
-            intentos -= 1
-            corazones=intentos_a_corazones(intentos)
+            mostrar_ahorcado(intentos) 
+            print(f"¡Perdiste!💀 La palabra era: {palabra}")
 
-            print(f"¡Letra incorrecta! Te quedan {corazones} vidas.")
-        
-        if "_ " not in guiones:
-            print(f"¡Felicidades! Has adivinado la palabra: {palabra}")
-            break
-    else:
-        print(f"¡Perdiste! La palabra era: {palabra}")
+        jugar_otra_vez = input("\nPresiona Enter para jugar de nuevo o escribe 'salir' para terminar: ")
+        if jugar_otra_vez.lower() == 'salir':
+            print("¡Gracias por jugar! 😊")
+            break  
+
+    
 
 juego()
 
-#TODO: Mensajes en otro archivo, cuerpito del tipito, readme, play again?, clases en otro archivo, tu categoria es.
+#TODO: Mensajes en otro archivo, readme, clases en otro archivo, tu categoria es.
